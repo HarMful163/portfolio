b9ef836774d15eec372511a7348f9e34658afeb1
Nikita Bogomolov <bogomolovnikita163@gmail.com>
2021-12-23 19:04:17 +0300

    finished adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -51,22 +51,77 @@ window.addEventListener('DOMContentLoaded', function() {
     });
     buttonContacts.forEach(function(e) {
         e.addEventListener('click', () => {
-            scrollTo(price);
+            scrollTo(contacts);
         })
     });
     buttonPrice.forEach(function(e) {
         e.addEventListener('click', () => {
-            scrollTo(contacts);
+            scrollTo(price);
         })
     });
     
 
-    //section _skills.
+    //_skills procents.
 
     const counters = document.querySelectorAll('.skills__item-percent'),
-    lines = document.querySelectorAll('.skills__item-scale span');
+          lines = document.querySelectorAll('.skills__item-scale span');
 
     counters.forEach( (item, i) => {
         lines[i].style.width = item.innerHTML;
     });
+
+    // _skills cards
+
+    const buttonOpen = document.querySelectorAll('.skills__skill-open'),
+          buttonClose = document.querySelectorAll('.skills__skill-close'),
+          skillWrapper = document.querySelectorAll('.skills__skill-wrapper'),
+          skillList = document.querySelectorAll('.skills__skill-list');
+        
+        
+
+        function showList() {
+            skillWrapper.forEach((item) => {
+                item.classList.toggle('skills__skill-wrapper-active');
+            });
+
+            skillList.forEach((item) => {
+                item.classList.toggle('skills__skill-list-active');
+            });
+        }
+
+        function showWrapper() {
+            skillWrapper.forEach((item) => {
+                item.classList.toggle('skills__skill-wrapper-active');
+            });
+
+            skillList.forEach((item) => {
+                item.classList.toggle('skills__skill-list-active');
+            });
+        }
+
+
+        buttonOpen.forEach((btn) => {
+            btn.addEventListener('click', showList);
+        })
+
+        buttonClose.forEach((btn) => {
+            btn.addEventListener('click', showWrapper);
+        })
+
+    
+
+    //menu 
+
+    const hamburger = document.querySelector('.hamburger'),
+          menu = document.querySelector('.menu'),
+          closeElem = document.querySelector('.menu__close');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+
+    closeElem.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+    
 });
